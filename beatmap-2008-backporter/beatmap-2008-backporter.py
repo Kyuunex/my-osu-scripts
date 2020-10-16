715cf2f159ee81f163be2d694c97fdb0db9d417f
Kyuunex <kyuunex@protonmail.ch>
2020-10-17 00:29:25 +0400

    come cleanup

--- beatmap-2008-backporter/beatmap-2008-backporter.py
+++ beatmap-2008-backporter/beatmap-2008-backporter.py
@@ -18,6 +18,8 @@ class Beatmap:
         self.colours_section = []
         self.hit_objects_section = []
 
+        self.parse()
+
     def parse(self):
         current_section = ""
 
@@ -151,76 +153,86 @@ class Beatmap:
         print("File saved!")
 
 
-def convert_timing_points(timing_points_section):
-    timing_points = []
-    base_beat_length = 0
-    for current_timing_point in timing_points_section:
-        time, beat_length, meter, sample_set, sample_index, volume, uninherited, effects = current_timing_point
-
-        if uninherited == "1":
-            # red line
-            base_beat_length = float(beat_length)
-            timing_points.append([time, beat_length, meter, sample_set, sample_index, volume, "1", effects])
-        elif uninherited == "0":
-            # green line
-            new_beat_length = round(base_beat_length * abs(float(beat_length)) / 100, 12)
-            timing_points.append([time, str(new_beat_length), meter, sample_set, sample_index, volume, "1", effects])
-
-    return timing_points
-
-
-def convert_diff_settings(difficulty_section):
-    new_difficulty_settings = {}
-    for difficulty_line in difficulty_section.items():
-        diff_setting = difficulty_line[0]
-
-        if diff_setting == "OverallDifficulty":
-            new_difficulty_settings["OverallDifficulty"] = str(int(float(difficulty_line[1])))
-        elif diff_setting == "ApproachRate":
-            new_difficulty_settings["OverallDifficulty"] = str(int(float(difficulty_line[1])))
-        elif diff_setting == "HPDrainRate":
-            new_difficulty_settings["HPDrainRate"] = str(int(float(difficulty_line[1])))
-        elif diff_setting == "CircleSize":
-            new_difficulty_settings["CircleSize"] = str(int(float(difficulty_line[1])))
-        elif diff_setting == "SliderMultiplier":
-            new_difficulty_settings["SliderMultiplier"] = str(float(difficulty_line[1]))
-        elif diff_setting == "SliderTickRate":
-            new_difficulty_settings["SliderTickRate"] = str(int(float(difficulty_line[1])))
-
-    return new_difficulty_settings
-
-
-def fix_nonexistant_drum_sample(beatmap):
-    beatmap.general_section["SampleSet"] = "None"
-
-
-def fix_max_sv(beatmap):
-    timing_points = []
-    slider_multiplier = float(beatmap.difficulty_section['SliderMultiplier'])
-    if slider_multiplier > 2.6:
-        slider_multiplier = slider_multiplier / 2
-        beatmap.difficulty_section['SliderMultiplier'] = str(slider_multiplier)
-
-        for current_timing_point in beatmap.timing_points_section:
+class BeatmapBackporter(Beatmap):
+    def convert_timing_points(self):
+        new_timing_points = []
+        base_beat_length = 0
+        for current_timing_point in self.timing_points_section:
             time, beat_length, meter, sample_set, sample_index, volume, uninherited, effects = current_timing_point
 
-            timing_points.append([time, str(float(beat_length) / 2), meter, sample_set,
-                                  sample_index, volume, uninherited, effects])
+            if uninherited == "1":
+                # red line
+                base_beat_length = float(beat_length)
+                new_timing_points.append([time, beat_length, meter, sample_set, sample_index, volume, "1", effects])
+            elif uninherited == "0":
+                # green line
+                new_beat_length = round(base_beat_length * abs(float(beat_length)) / 100, 12)
+                new_timing_points.append([time, str(new_beat_length), meter, sample_set,
+                                          sample_index, volume, "1", effects])
 
-        beatmap.timing_points_section = timing_points
+        self.timing_points_section = new_timing_points
+
+    def convert_diff_settings(self):
+        new_difficulty_settings = {}
+        for difficulty_line in self.difficulty_section.items():
+            diff_setting = difficulty_line[0]
+
+            if diff_setting == "OverallDifficulty":
+                new_difficulty_settings["OverallDifficulty"] = str(int(float(difficulty_line[1])))
+            elif diff_setting == "ApproachRate":
+                new_difficulty_settings["OverallDifficulty"] = str(int(float(difficulty_line[1])))
+            elif diff_setting == "HPDrainRate":
+                new_difficulty_settings["HPDrainRate"] = str(int(float(difficulty_line[1])))
+            elif diff_setting == "CircleSize":
+                new_difficulty_settings["CircleSize"] = str(int(float(difficulty_line[1])))
+            elif diff_setting == "SliderMultiplier":
+                new_difficulty_settings["SliderMultiplier"] = str(float(difficulty_line[1]))
+            elif diff_setting == "SliderTickRate":
+                new_difficulty_settings["SliderTickRate"] = str(int(float(difficulty_line[1])))
+
+        self.difficulty_section = new_difficulty_settings
+
+    def fix_nonexistant_drum_sample(self):
+        self.general_section["SampleSet"] = "None"
+
+    def fix_max_sv(self):
+        new_timing_points = []
+        slider_multiplier = float(self.difficulty_section['SliderMultiplier'])
+        if slider_multiplier > 2.6:
+            slider_multiplier = slider_multiplier / 2
+            self.difficulty_section['SliderMultiplier'] = str(slider_multiplier)
+
+            for current_timing_point in self.timing_points_section:
+                time, beat_length, meter, sample_set, sample_index, volume, uninherited, effects = current_timing_point
+
+                new_timing_points.append([time, str(float(beat_length) / 2), meter, sample_set,
+                                          sample_index, volume, uninherited, effects])
+
+            self.timing_points_section = new_timing_points
+
+        if slider_multiplier < 0.4:
+            slider_multiplier = slider_multiplier * 2
+            self.difficulty_section['SliderMultiplier'] = str(slider_multiplier)
+
+            for current_timing_point in self.timing_points_section:
+                time, beat_length, meter, sample_set, sample_index, volume, uninherited, effects = current_timing_point
+
+                new_timing_points.append([time, str(float(beat_length) * 2), meter, sample_set,
+                                          sample_index, volume, uninherited, effects])
+
+            self.timing_points_section = new_timing_points
 
 
 del sys.argv[0]
 
 
 def convert(file_to_convert):
-    a = Beatmap(file_to_convert)
-    a.parse()
+    a = BeatmapBackporter(file_to_convert)
     if a.osu_file_format_version > 5:
-        a.timing_points_section = convert_timing_points(a.timing_points_section)
-        a.difficulty_section = convert_diff_settings(a.difficulty_section)
-        fix_max_sv(a)
-        fix_nonexistant_drum_sample(a)
+        a.convert_timing_points()
+        a.convert_diff_settings()
+        a.fix_max_sv()
+        a.fix_nonexistant_drum_sample()
         a.osu_file_format_version = 5
         a.save_file()
         print("saved " + file_to_convert)
@@ -235,13 +247,12 @@ if not sys.argv:
         if beatmap_folder == "..":
             continue
 
-        for file in os.listdir(os.getcwd()+"/"+beatmap_folder):
+        for file in os.listdir(os.getcwd() + "/" + beatmap_folder):
             if file.endswith(".osu"):
-                convert(os.getcwd()+"/"+beatmap_folder+"/"+file)
+                convert(os.getcwd() + "/" + beatmap_folder + "/" + file)
 elif "MAPSET" in sys.argv:
     for file in os.listdir(os.getcwd()):
         if file.endswith(".osu"):
-            convert(os.getcwd()+"/"+file)
+            convert(os.getcwd() + "/" + file)
 else:
     convert(" ".join(sys.argv))
-
